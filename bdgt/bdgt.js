771b3214f65f9e455876a52a141aff06c61c5d22
charlie8230 <carlos@moranjr.com>
2015-06-02 01:19:49 -0400

    work

--- bdgt/bdgt.js
+++ bdgt/bdgt.js
@@ -44,26 +44,42 @@ if (Meteor.isServer) {
       return 'hi ' + name;
     }
   });
+
+  Comments.allow({
+    insert: function(userId, doc) {
+      return !!userId;
+    },
+    update: function(userId, doc) {
+      return false;
+    },
+    remove: function(userId, doc) {
+      return false;
+    }
+  });
   // returns a cursor that describes the data
   Meteor.publish('allComments', function () {
     var cursor = Comments.find();
-    var self = this;
-    var handle = cursor.observeChanges({
-      added: function (id, fields) {
-        self.added('comments', id, fields);
-      },
-      changed: function (id, fields) {
-        self.changed('comments', id, fields);
-      },
-      removed: function (id) {
-        self.removed('comments', id);
-      }
-    });
-
-    this.ready();
-    this.onStop(function () {
-      handle.stop();
-    });
-    //return cursor;
+    // var self = this;
+    // var handle = cursor.observeChanges({
+    //   added: function (id, fields) {
+    //     self.added('comments', id, fields);
+    //   },
+    //   changed: function (id, fields) {
+    //     self.changed('comments', id, fields);
+    //   },
+    //   removed: function (id) {
+    //     self.removed('comments', id);
+    //   }
+    // });
+    //
+    // this.ready();
+    // this.onStop(function () {
+    //   handle.stop();
+    // });
+    if(this.userId) {
+      return cursor;
+    } else {
+      return this.ready();
+    }
   });
 }
